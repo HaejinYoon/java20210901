55ba08f5c2152c96717fe23b3176ca482a50830f
Haejin Yoon <hjyoomp@gmail.com>
2021-09-15 16:54:50 +0900

    210915 3rd lecture commit

--- src/p15collection/p02quiz/Ex01.java
+++ src/p15collection/p02quiz/Ex01.java
@@ -10,16 +10,28 @@ public class Ex01 {
 		
 		boolean run = true;
 		while(run) {
-			System.out.print("l:list, e:exit 입력>");
+			System.out.print("l:list, e:exit, r:remove 입력>");
 			String s = scan.next();
 			if(s.equals("l")) {
 				for(int i=0;i<arr.size();i++) {
 					System.out.println(arr.get(i));
 				}
 			} else if(s.equals("e")) {
+				System.out.println("프로그램을 종료합니다.");
 				break;
-			} else {
-				arr.add(s);				
+			} else if(s.equals("r")) {
+				//if(arr.size()>0) {
+				if(!arr.isEmpty()) {
+					System.out.println(arr.get(arr.size()-1)+" 정상적으로 삭제되었습니다");
+					arr.remove(arr.size()-1);
+				}else {
+					System.out.println("아직 아무것도 없습니다");
+				}
+				
+			}
+			else {
+				arr.add(s);		
+				System.out.println("정상적으로 추가되었습니다.");
 			}
 		}
 		scan.close();
---
+++ src/p15collection/p02quiz/Ex02.java
@@ -0,0 +1,107 @@
+package p15collection.p02quiz;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Integer> arr = new ArrayList<>();
+		boolean run = true;
+		ex:while(run) {
+			System.out.print("0:Exit, 1:Print, 2:Delete, 3:Total Score(50 above)> ");
+			int a=0;
+			try {
+				a=scan.nextInt();	
+			} catch (Exception e) {
+				System.out.println("input only 0 ~ 3 or 50 above");
+			}
+			switch (a) {
+			case 0:
+				System.out.println("Program Terminated");
+				break ex ;
+			case 1:
+				if(arr.size()==0) {
+					System.out.println("Nothing in here yet");
+				}	
+				for(int i=0;i<arr.size();i++) {
+					System.out.println(arr.get(i));
+				}
+				break;
+			case 2:
+				if(arr.size()>0) {
+					System.out.println(arr.get(arr.size()-1)+" deleted");
+					arr.remove(arr.size()-1);
+				}
+				break;
+			case 3:
+				int sum =0;
+				double avg =0;
+				if(arr.size()>0) {
+					for(int i=0;i<arr.size();i++) {
+						sum +=arr.get(i);
+					}
+				} else {
+					System.out.println("Nothing in here yet");
+				}
+				avg=sum/(double)arr.size();
+				System.out.println("Total: "+sum);
+				System.out.println("Average: "+avg);
+				
+				int max = max(arr);
+				int min = min(arr);
+
+				System.out.println("Max: "+max);
+				System.out.println("Min: "+min);
+				break;
+			default:
+				if(a>=50) {
+					arr.add(a);
+					System.out.println("Input correctly");
+					break;
+				}else if(a!=1){
+					System.out.println("Score must be larger than 50!");
+					break;
+				}else {
+					break;
+				}
+			}
+		}
+		scan.close();
+	}
+
+
+
+	private static int min(ArrayList<Integer> list) {
+		if (list.isEmpty()) {
+			return 0;
+		}
+		
+		int min = list.get(0);
+		
+		for (int i = 1; i < list.size(); i++) {
+			if (min > list.get(i)) {
+				min = list.get(i);
+			}
+		}
+		
+		return min;
+	}
+
+	private static int max(ArrayList<Integer> list) {
+		if (list.isEmpty()) {
+			return 0;
+		}
+		
+		int max = list.get(0);
+		
+		for (int i = 1; i < list.size(); i++) {
+			if (max < list.get(i)) {
+				max = list.get(i);
+			}
+		}
+		
+		return max;
+	}
+}
